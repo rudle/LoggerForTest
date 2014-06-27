0a1b32aa795f5ad920668695ea5cd27c1d089156
Sean Sorrell <ssorrell@squareup.com>
2014-06-26 18:22:05 -0700

    Initial implementation of LoggerForTest

--- lib/logger_for_test.rb
+++ lib/logger_for_test.rb
@@ -1,5 +1,26 @@
-require "logger_for_test/version"
+require 'delegate'
 
-module LoggerForTest
-  # Your code goes here...
+# This is the implementation of Logger that stores an in memory representation
+# of your log lines. It uses the delegator pattern to ensure that things
+# continue to work
+# suggested usages:
+# LoggerForTest.new(Logger.new(STDOUT))
+# LoggerForTest.new(Rails.logger))
+class LoggerForTest < SimpleDelegator
+  def initialize(*args)
+    super
+    @log_lines = []
+  end
+  attr_accessor :log_lines
+
+  def add(sev, message = nil, progname = nil)
+    log_lines << format(sev, message, progname)
+    super(sev, message, progname)
+  end
+
+  def format(sev, message, progname)
+    { sev: sev,
+      message: message,
+      progname: progname }
+  end
 end
---
+++ test/logger_for_test/logger_for_test_test.rb
@@ -0,0 +1,29 @@
+require_relative '../test_helper'
+require_relative '../../lib/logger_for_test.rb'
+
+describe LoggerForTest do
+  before do
+    @logger_double = MiniTest::Mock.new
+    @logger = LoggerForTest.new(@logger_double)
+  end
+
+  describe '#add' do
+    it 'shall call the implementation of Logger' do
+      @logger_double.expect(:add, nil,
+                            ['error', 'something went wrong', 'progname'])
+
+      @logger.add('error', 'something went wrong', 'progname')
+    end
+
+    it 'shall log the input in memory' do
+      @logger_double.expect(:add, nil,
+                            ['error', 'something went wrong', 'progname'])
+
+      @logger.add('error', 'something went wrong', 'progname')
+
+      @logger.log_lines.must_equal [{ sev: 'error',
+                                      message: 'something went wrong',
+                                      progname: 'progname' }]
+    end
+  end
+end
